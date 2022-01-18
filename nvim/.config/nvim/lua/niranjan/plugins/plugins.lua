517bd0be36a9bcc2f8af38fd8ac7433ff7192a80
Neragin <niranjan@nmathirajan.xyz>
2022-01-17 19:37:51 -0800

    added zsh-aliases

--- nvim/.config/nvim/lua/niranjan/plugins/plugins.lua
+++ nvim/.config/nvim/lua/niranjan/plugins/plugins.lua
@@ -45,7 +45,6 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
   use "kyazdani42/nvim-web-devicons"
-  use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
   use {
       'goolord/alpha-nvim',
       config = function ()
